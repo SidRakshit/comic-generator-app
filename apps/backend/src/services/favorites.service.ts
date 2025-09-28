e16edf8b8c41660f9c9a5a4e7023a08d7520f6e3
SidRakshit <sidrakshit5@gmail.com>
2025-09-28 13:02:01 -0400

    new day, new attempt 3

--- apps/backend/src/services/favorites.service.ts
+++ apps/backend/src/services/favorites.service.ts
@@ -1,6 +1,4 @@
-import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient();
+import prisma from "../database/prisma";
 
 export class FavoritesService {
   async getFavoriteComics(userId: string) {
